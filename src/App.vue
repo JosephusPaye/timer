83da0063ecf922f5483dc47dea60552a93794b4d
Josephus Paye II <j.paye96@gmail.com>
2020-08-24 11:33:32 +1000

    WIP: Add demo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,97 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div id="app">
+        <Timer
+            ref="timer"
+            allowOverflow
+            :length="5 * 1000"
+            @done="onDone"
+            @start="onStart"
+            @reset="onReset"
+            @pause="onPause"
+            @resume="onResume"
+            v-slot="{ time, state, timeElapsed, isOverflowed, isDone }"
+        >
+            <div>
+                <div
+                    :style="{
+                        color: isOverflowed ? 'red' : 'black',
+                        fontFamily: 'monospace',
+                    }"
+                >
+                    {{ time.h }}:{{ time.m }}:{{ time.s }}:{{ time.ms }}
+                </div>
+                <button @click="toggle">
+                    {{
+                        state === 'stopped'
+                            ? 'Start'
+                            : state === 'running'
+                            ? 'Pause'
+                            : 'Resume'
+                    }}
+                </button>
+                <button @click="reset">Reset</button>
+                <code
+                    ><pre
+                        v-text="
+                            JSON.stringify({
+                                state,
+                                time,
+                                timeElapsed,
+                                isOverflowed,
+                                isDone,
+                            }, null, '  ')
+                        "
+                    ></pre
+                ></code>
+            </div>
+        </Timer>
+    </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import { TimerVue as Timer } from '../lib/dist/timer.esm.js';
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        Timer,
+    },
+    methods: {
+        toggle() {
+            this.$refs.timer.toggle();
+        },
+
+        reset() {
+            this.$refs.timer.reset();
+        },
+
+        onStart() {
+            console.log('onStart');
+        },
+
+        onReset(time) {
+            console.log('onReset', time);
+        },
+
+        onPause() {
+            console.log('onPause');
+        },
+
+        onResume() {
+            console.log('onResume');
+        },
+
+        onDone() {
+            console.log('onDone');
+        },
+    },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    color: #2c3e50;
+    font-size: 18px;
+    margin-top: 60px;
 }
 </style>
